f3811f389a178c5f621a262ddf3126964bdc1d3b
mariusz <mariuszjurczenko@gmail.com>
2020-12-06 15:43:17 +0100

    Add Web API

---
+++ WebExampleSolid/Controllers/CarWashController.cs
@@ -0,0 +1,25 @@
+﻿using ExampleSolid;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebExampleSolid.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarWashController : ControllerBase
+    {
+        private CarWash _carWash;
+
+        public CarWashController(CarWash carWash)
+        {
+            _carWash = carWash;
+        }
+
+        [HttpPost()]
+        public ActionResult<decimal> Pracing()
+        {      
+            _carWash.Pricing();
+
+            return _carWash.WashingCost;
+        }
+    }
+}
